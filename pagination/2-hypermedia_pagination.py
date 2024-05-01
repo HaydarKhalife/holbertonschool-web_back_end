1a03d8a6640871a2f8e7a17fed1d0b94e4019a66
Haydar <146635050+HaydarKhalife@users.noreply.github.com>
2024-05-01 21:40:51 +0300

    Update 2-hypermedia_pagination.py

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
-""" Pagination """
+"""
+__Summary__
+2 Hypermedia pagination
+Replicate code from the previous task.
 
+Classes:
+    Server
+
+Functions:
+    dataset(self) -> List[List]
+    get_page(self, int, int) -> List[List]
+    index_range(self, int, int) -> Tuple[int, int]
+    get_hyper(self, int, int) -> dict
+
+"""
 import csv
 import math
-from typing import List, Tuple, Dict, Any
+from typing import List, Dict
+index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
@@ -26,11 +40,30 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        pass
+        """Return page of the dataset
+        """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        page, page_size = index_range(page, page_size)
+        try:
+            return self.dataset()[page:page_size]
+        except IndexError:
+            return []
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
-        pass
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Return page of the dataset"""
 
-    def index_range(page: int, page_size: int) -> Tuple[int, int]:
-        """ returns tuple of size two containing start index, end index """
-        pass
+        listpage = self.get_page(page, page_size)
+
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        prev_page = page - 1 if page > 1 else None
+        next_page = page + 1 if page < total_pages else None
+        page_size = 0 if listpage == [] else page_size
+
+        return {'page_size': len(listpage),
+                'page': page,
+                'data': listpage,
+                'next_page': next_page,
+                'prev_page': prev_page,
+                'total_pages': total_pages}
